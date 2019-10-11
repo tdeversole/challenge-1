2e34e4c59b68024f67c18b3f911b49a3ce683b79
Tim D Eversole <tdeversole@yahoo.com>
2019-10-11 14:06:36 -0600

    tst

--- challenge1.js
+++ challenge1.js
@@ -2,6 +2,7 @@ require('chromedriver');
 var webdriver = require('selenium-webdriver');
 var assert = require("chai").assert;
 
+
 describe("challenge1 suite", function(){
     this.timeout(20000);
     var driver;
