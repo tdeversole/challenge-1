3476c335edecdb9868c5d1b0bc4ebe77a515665b
Tim D Eversole <56331013+tdeversole@users.noreply.github.com>
2019-10-11 14:10:08 -0600

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -2,7 +2,6 @@ require('chromedriver');
 var webdriver = require('selenium-webdriver');
 var assert = require("chai").assert;
 
-
 describe("challenge1 suite", function(){
     this.timeout(20000);
     var driver;
